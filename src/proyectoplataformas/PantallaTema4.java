ba1fb98b01d4d833279ea70ea5eb670de32fc32b
Camilo Mora <camiloalf23@gmail.com>
2023-11-22 14:03:02 -0500

    Agregamos contenido a pantalla tema4

--- src/proyectoplataformas/PantallaTema4.java
+++ src/proyectoplataformas/PantallaTema4.java
@@ -12,6 +12,17 @@ public class PantallaTema4 extends javax.swing.JPanel {
 
         jPanel1 = new javax.swing.JPanel();
         titulo_tema4 = new javax.swing.JLabel();
+        jLabel1_tema4 = new javax.swing.JLabel();
+        jLabel2_tema4 = new javax.swing.JLabel();
+        jLabel3_tema4 = new javax.swing.JLabel();
+        jLabel4_tema4 = new javax.swing.JLabel();
+        jLabel5_tema4 = new javax.swing.JLabel();
+        jLabel6_tema4 = new javax.swing.JLabel();
+        jLabel7_tema4 = new javax.swing.JLabel();
+        jLabel8_tema4 = new javax.swing.JLabel();
+        jLabel9_tema4 = new javax.swing.JLabel();
+        jLabel10_tema4 = new javax.swing.JLabel();
+        jLabel11_tema4 = new javax.swing.JLabel();
 
         setMaximumSize(new java.awt.Dimension(974, 485));
         setMinimumSize(new java.awt.Dimension(974, 485));
@@ -31,20 +42,153 @@ public class PantallaTema4 extends javax.swing.JPanel {
         titulo_tema4.setMinimumSize(new java.awt.Dimension(974, 48));
         titulo_tema4.setPreferredSize(new java.awt.Dimension(974, 48));
 
+        jLabel1_tema4.setFont(new java.awt.Font("JetBrains Mono ExtraBold", 0, 15)); // NOI18N
+        jLabel1_tema4.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel1_tema4.setText("● Transforming I/O Requests to Hardware Operations involves several steps");
+        jLabel1_tema4.setToolTipText("");
+        jLabel1_tema4.setMaximumSize(new java.awt.Dimension(860, 21));
+        jLabel1_tema4.setMinimumSize(new java.awt.Dimension(860, 21));
+        jLabel1_tema4.setPreferredSize(new java.awt.Dimension(860, 21));
+
+        jLabel2_tema4.setFont(new java.awt.Font("JetBrains Mono ExtraBold", 0, 15)); // NOI18N
+        jLabel2_tema4.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel2_tema4.setText("when, for example, reading a file from disk for a process:");
+        jLabel2_tema4.setToolTipText("");
+        jLabel2_tema4.setMaximumSize(new java.awt.Dimension(860, 21));
+        jLabel2_tema4.setMinimumSize(new java.awt.Dimension(860, 21));
+        jLabel2_tema4.setPreferredSize(new java.awt.Dimension(860, 21));
+
+        jLabel3_tema4.setFont(new java.awt.Font("JetBrains Mono", 0, 15)); // NOI18N
+        jLabel3_tema4.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel3_tema4.setText("> Determining the Device: Identify the device that holds the required file.");
+        jLabel3_tema4.setToolTipText("");
+        jLabel3_tema4.setMaximumSize(new java.awt.Dimension(860, 21));
+        jLabel3_tema4.setMinimumSize(new java.awt.Dimension(860, 21));
+        jLabel3_tema4.setPreferredSize(new java.awt.Dimension(860, 21));
+
+        jLabel4_tema4.setFont(new java.awt.Font("JetBrains Mono", 0, 15)); // NOI18N
+        jLabel4_tema4.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel4_tema4.setText("> Translating Name to Device Representation: Translate the file's name into a");
+        jLabel4_tema4.setToolTipText("");
+        jLabel4_tema4.setMaximumSize(new java.awt.Dimension(860, 21));
+        jLabel4_tema4.setMinimumSize(new java.awt.Dimension(860, 21));
+        jLabel4_tema4.setPreferredSize(new java.awt.Dimension(860, 21));
+
+        jLabel5_tema4.setFont(new java.awt.Font("JetBrains Mono", 0, 15)); // NOI18N
+        jLabel5_tema4.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel5_tema4.setText("format that the device understands, allowing access.");
+        jLabel5_tema4.setToolTipText("");
+        jLabel5_tema4.setMaximumSize(new java.awt.Dimension(860, 21));
+        jLabel5_tema4.setMinimumSize(new java.awt.Dimension(860, 21));
+        jLabel5_tema4.setPreferredSize(new java.awt.Dimension(860, 21));
+
+        jLabel6_tema4.setFont(new java.awt.Font("JetBrains Mono", 0, 15)); // NOI18N
+        jLabel6_tema4.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel6_tema4.setText("> Reading Data from Disk: Physically retrieve data from the disk and load");
+        jLabel6_tema4.setToolTipText("");
+        jLabel6_tema4.setMaximumSize(new java.awt.Dimension(860, 21));
+        jLabel6_tema4.setMinimumSize(new java.awt.Dimension(860, 21));
+        jLabel6_tema4.setPreferredSize(new java.awt.Dimension(860, 21));
+
+        jLabel7_tema4.setFont(new java.awt.Font("JetBrains Mono", 0, 15)); // NOI18N
+        jLabel7_tema4.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel7_tema4.setText("it into a buffer in memory.");
+        jLabel7_tema4.setToolTipText("");
+        jLabel7_tema4.setMaximumSize(new java.awt.Dimension(860, 21));
+        jLabel7_tema4.setMinimumSize(new java.awt.Dimension(860, 21));
+        jLabel7_tema4.setPreferredSize(new java.awt.Dimension(860, 21));
+
+        jLabel8_tema4.setFont(new java.awt.Font("JetBrains Mono", 0, 15)); // NOI18N
+        jLabel8_tema4.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel8_tema4.setText("> Making Data Available: Once the data is in the buffer, it becomes");
+        jLabel8_tema4.setToolTipText("");
+        jLabel8_tema4.setMaximumSize(new java.awt.Dimension(860, 21));
+        jLabel8_tema4.setMinimumSize(new java.awt.Dimension(860, 21));
+        jLabel8_tema4.setPreferredSize(new java.awt.Dimension(860, 21));
+
+        jLabel9_tema4.setFont(new java.awt.Font("JetBrains Mono", 0, 15)); // NOI18N
+        jLabel9_tema4.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel9_tema4.setText("accessible to the requesting process.");
+        jLabel9_tema4.setToolTipText("");
+        jLabel9_tema4.setMaximumSize(new java.awt.Dimension(860, 21));
+        jLabel9_tema4.setMinimumSize(new java.awt.Dimension(860, 21));
+        jLabel9_tema4.setPreferredSize(new java.awt.Dimension(860, 21));
+
+        jLabel10_tema4.setFont(new java.awt.Font("JetBrains Mono", 0, 15)); // NOI18N
+        jLabel10_tema4.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel10_tema4.setText("> Returning Control to Process: Finally, control is handed back to the");
+        jLabel10_tema4.setToolTipText("");
+        jLabel10_tema4.setMaximumSize(new java.awt.Dimension(860, 21));
+        jLabel10_tema4.setMinimumSize(new java.awt.Dimension(860, 21));
+        jLabel10_tema4.setPreferredSize(new java.awt.Dimension(860, 21));
+
+        jLabel11_tema4.setFont(new java.awt.Font("JetBrains Mono", 0, 15)); // NOI18N
+        jLabel11_tema4.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel11_tema4.setText("process, which can then utilize the data retrieved from the disk.");
+        jLabel11_tema4.setToolTipText("");
+        jLabel11_tema4.setMaximumSize(new java.awt.Dimension(860, 21));
+        jLabel11_tema4.setMinimumSize(new java.awt.Dimension(860, 21));
+        jLabel11_tema4.setPreferredSize(new java.awt.Dimension(860, 21));
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(56, 56, 56)
+                .addComponent(jLabel1_tema4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(titulo_tema4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap())
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addComponent(titulo_tema4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addContainerGap())
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(jLabel7_tema4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel9_tema4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel11_tema4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel5_tema4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(21, 21, 21))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel6_tema4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                .addComponent(jLabel3_tema4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addComponent(jLabel2_tema4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addComponent(jLabel4_tema4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addComponent(jLabel8_tema4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel10_tema4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(32, 32, 32))))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addComponent(titulo_tema4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 437, Short.MAX_VALUE))
+                .addGap(28, 28, 28)
+                .addComponent(jLabel1_tema4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel2_tema4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(jLabel3_tema4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel4_tema4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel5_tema4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel6_tema4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel7_tema4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel8_tema4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel9_tema4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel10_tema4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel11_tema4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 106, Short.MAX_VALUE))
         );
 
         add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
@@ -52,6 +196,17 @@ public class PantallaTema4 extends javax.swing.JPanel {
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel10_tema4;
+    private javax.swing.JLabel jLabel11_tema4;
+    private javax.swing.JLabel jLabel1_tema4;
+    private javax.swing.JLabel jLabel2_tema4;
+    private javax.swing.JLabel jLabel3_tema4;
+    private javax.swing.JLabel jLabel4_tema4;
+    private javax.swing.JLabel jLabel5_tema4;
+    private javax.swing.JLabel jLabel6_tema4;
+    private javax.swing.JLabel jLabel7_tema4;
+    private javax.swing.JLabel jLabel8_tema4;
+    private javax.swing.JLabel jLabel9_tema4;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JLabel titulo_tema4;
     // End of variables declaration//GEN-END:variables
